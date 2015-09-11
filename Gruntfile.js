13fb201750cd506cc978352a03d992f6d8a12539
Sher Minn Chong <sherminn.chong@gmail.com>
2015-09-11 15:52:08 -0400

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            files: ['**/*.html', '**/*.js', '**/*.css'],
+            options: {
+                livereload: {
+                    port: 9011
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-serve');
+}
